fa1d755d670d5b83969c917f2dd036d8cb028ad7
Johnny O'Donnell <odjohnny@amazon.com>
2020-02-24 20:42:09 -0800

    Initial commit
    Read file into string
    Create Environment class

---
+++ lib/Environment.pm
@@ -0,0 +1,35 @@
+package Environment;
+
+use strict;
+
+
+sub new {
+    my ($class, $map, $parent)  = @_;
+
+    bless { map => $map, parent => $parent }, $class;
+};
+
+sub get {
+    my ($self, $key) = @_;
+
+    if (exists $self->{map}{$key}) {
+        $self->{map}{$key};
+    } elsif (defined $self->{parent}) {
+        $self->{parent}->get($key);
+    }
+}
+
+sub put : lvalue {
+    my ($self, $key, $value) = @_;
+
+    if ($value) {
+        $self->{map}{$key} = $value;
+    } else {
+        $self->{map}{$key};
+    }
+}
+
+our $global = Environment->new({
+        "key" => "value"
+    });
+
---
+++ test-scripts/fibo.scm
@@ -0,0 +1,9 @@
+
+(define fib
+  (lambda (n)
+    (if (< n 3)
+      1
+      (+ (fib (- n 1)) (fib (- n 2))))))
+
+(print (fib 10))
+
